1e29b4a9930209441b8ddd246569a2676979f68b
oskulaurila <olaurila@abo.fi>
2019-01-14 02:05:23 +0200

    Updates

---
+++ precache-manifest.1ea320891d0964d27a139f99db549ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca78421ba38415f0b618",
+    "url": "/2018-interactive-web-apps-project-oskulaurila/static/js/runtime~main.ca78421b.js"
+  },
+  {
+    "revision": "9d7468d992130d7f6011",
+    "url": "/2018-interactive-web-apps-project-oskulaurila/static/js/main.9d7468d9.chunk.js"
+  },
+  {
+    "revision": "b4cbf78dea05d52b227a",
+    "url": "/2018-interactive-web-apps-project-oskulaurila/static/js/1.b4cbf78d.chunk.js"
+  },
+  {
+    "revision": "9d7468d992130d7f6011",
+    "url": "/2018-interactive-web-apps-project-oskulaurila/static/css/main.a66dc4a1.chunk.css"
+  },
+  {
+    "revision": "b4cbf78dea05d52b227a",
+    "url": "/2018-interactive-web-apps-project-oskulaurila/static/css/1.d8abcc86.chunk.css"
+  },
+  {
+    "revision": "353326c4042ec4bf46840a7cdba23cfb",
+    "url": "/2018-interactive-web-apps-project-oskulaurila/index.html"
+  }
+];
